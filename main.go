c9a2dd41b18525ebf83ecefb7264dfc6931767ed
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-02 19:08:22 +1100

    feat: initialised TCP server listen for connections and response with OK

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"io"
+	"net"
+	"os"
 )
 
 func main() {
-  fmt.Println("Hellow World")
+  fmt.Println("Listening on port :6379")
+  // start TCP Listen
+  l, err := net.Listen("tcp", ":6379")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Listen for connections
+  conn, err := l.Accept()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  defer conn.Close()
+
+  for {
+    buf := make([]byte, 1024)
+
+    _, err = conn.Read(buf)
+    if err != nil {
+      if err == io.EOF {
+        break
+      }
+      fmt.Println("error reading from client: ", err.Error())
+      os.Exit(1)
+    }
+
+    conn.Write([]byte("+OK\r\n"))
+
+  }
 }
