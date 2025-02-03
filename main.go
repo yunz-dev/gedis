24b9c681dc49db0c0c7e983fdee2985792e85dfd
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-03 23:19:12 +1100

    feat: updated main with new RESP functions

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
@@ -26,16 +24,13 @@ func main() {
   defer conn.Close()
 
   for {
-    buf := make([]byte, 1024)
-
-    _, err = conn.Read(buf)
+    resp := NewResp(conn)
+    value, err := resp.Read()
     if err != nil {
-      if err == io.EOF {
-        break
-      }
-      fmt.Println("error reading from client: ", err.Error())
-      os.Exit(1)
+      fmt.Println(err)
+      return
     }
+    fmt.Println(value)
 
     conn.Write([]byte("+OK\r\n"))
 
