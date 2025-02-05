ebab4d4b4c369687fc28ef36cc6b75cf2d1db6a8
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-05 13:50:26 +1100

    feat: added writer struct and methid

--- resp.go
+++ resp.go
@@ -178,3 +178,22 @@ func (v Value) marshallError() []byte {
 func (v Value) marshallNull() []byte {
 	return []byte("$-1\r\n")
 }
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{writer: w}
+}
+
+func (w *Writer) Write(v Value) error {
+	var bytes = v.Marshal()
+
+	_, err := w.writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
