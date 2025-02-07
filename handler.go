aa032f3ded0aa15aa7a5be119322898033080958
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-08 00:29:16 +1100

    feat: added hget command

--- handler.go
+++ handler.go
@@ -73,3 +73,22 @@ func hset(args []Value) Value {
 
   return Value{typ: "string", str: "OK"}
 }
+
+func hget(args []Value) Value {
+  if len(args) != 2 {
+    return Value{typ: "error", str: "ERR wrong number of arguments for HSET command"}
+  }
+
+  hash := args[0].bulk
+  key := args[1].bulk
+
+  HSETsMu.RLock()
+  value, ok := HSETs[hash][key]
+  HSETsMu.RUnlock()
+
+  if !ok {
+    return Value{typ: "null"}
+  }
+
+  return Value{typ: "bulk", bulk: value}
+}
