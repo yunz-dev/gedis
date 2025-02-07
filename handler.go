455945b8f7e570e09e99c3595ef073a087e40c11
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-08 00:27:23 +1100

    feat: added HSET

--- handler.go
+++ handler.go
@@ -51,3 +51,25 @@ func get(args []Value) Value {
 
   return Value{typ: "bulk", bulk: value}
 }
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+  if len(args) != 3 {
+    return Value{typ: "error", str: "ERR wrong number of arguments for HSET command"}
+  }
+
+  hash := args[0].bulk
+  key := args[1].bulk
+  value := args[2].bulk
+
+  HSETsMu.Lock()
+  if _, ok := HSETs[hash]; !ok {
+    HSETs[hash] = map[string]string{}
+  }
+  HSETs[hash][key] = value
+  HSETsMu.Unlock()
+
+  return Value{typ: "string", str: "OK"}
+}
