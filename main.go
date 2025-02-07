130c43c84a0a825058223870731481e3b918e93b
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-08 00:44:45 +1100

    feat: added aof to main function

--- main.go
+++ main.go
@@ -15,6 +15,13 @@ func main() {
     return
   }
 
+  aof, err := NewAof("database.aof")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer aof.Close()
+
   // Listen for connections
   conn, err := l.Accept()
   if err != nil {
@@ -52,6 +59,10 @@ func main() {
       fmt.Println("Invalid commands: ", command)
       writer.Write(Value{typ: "string", str: ""})
       continue
+    }
+
+    if command == "SET" || command == "HSET" {
+      aof.Write(value)
     }
 
     result := handler(args)
